192416910680e4c288ab5ca4e5c7ffc74ca007b3
Christopher Palmer <snly2386@gmail.com>
2014-06-02 18:48:20 -0500

    add video vew

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,12 @@
 class VideosController < ApplicationController
   def create
-
+    @video = Video.create(:title => params[:title], :youtube_id => params[:youtube_id], :description => params[:description])
+    @user_id = params[:user_id]
+    redirect_to "/users/#{@user_id}/videos/#{@video.id}"
   end
 
   def show
-
+    @video = Video.find(params[:id])
   end
 
   def index
