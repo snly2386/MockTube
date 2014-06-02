360a17289398470cdfeb470fe881dfc2108d1e9a
Christopher Palmer <snly2386@gmail.com>
2014-06-02 17:45:37 -0500

    add video controller

---
+++ db/migrate/20140602215201_add_user_idto_video.rb
@@ -0,0 +1,5 @@
+class AddUserIdtoVideo < ActiveRecord::Migration
+  def change
+    add_column :videos, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602214136) do
+ActiveRecord::Schema.define(version: 20140602215201) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140602214136) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
