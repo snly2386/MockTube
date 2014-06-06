740ae5fbc9aa0af30175746d8954c22907970b37
Christopher Palmer <snly2386@gmail.com>
2014-06-06 16:57:49 -0500

    add screenshots

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,7 @@ class CommentsController < ApplicationController
     @comment = Comment.create(:video_id => params[:video_id], :content => params[:content], :user_id => params[:user_id])
     @video = Video.find(params[:video_id])
     @user = User.find(params[:user_id])
-    # flash[:success_comment] = "Comment Successfully Created"
-    # redirect_to "/videos/#{@video.id}"
+    
 
     respond_to do |format| 
       format.html {redirect_to "/users/#{@user.id}/videos/#{@video.id}", flash: {success_comment: "Comment Successfully Created"}}
